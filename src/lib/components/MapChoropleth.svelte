8c2484f46af142f1018cf55b6d4c0109c46b211e
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2024-11-22 10:57:54 +0100

    Update MapChoropleth.svelte

--- src/lib/components/MapChoropleth.svelte
+++ src/lib/components/MapChoropleth.svelte
@@ -19,7 +19,7 @@
 	import { min, max } from 'd3-array';
 
 	import { scaleQuantile, scaleSequential, scaleSequentialQuantile } from 'd3-scale';
-	import { schemeBlues, schemePuBu, schemeGnBu, schemeOrRd, schemeYlGn } from 'd3-scale-chromatic';
+	import { schemeBlues, schemePurples, schemeGnBu, schemeOrRd, schemeYlGn } from 'd3-scale-chromatic';
 	import { PuBlueDarker } from '$lib/utils/customColorPalettes';
 
 	import { formatInt } from '$lib/utils/formatNumbers';
